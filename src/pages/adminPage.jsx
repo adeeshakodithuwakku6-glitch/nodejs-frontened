e2d6f45506221961b3d915a2ed056b59de18f1d0
Adeesha Kodithuwakku <adeeshakodithuwakku6@gmail.com>
2026-07-17 23:38:16 +0530

    Day 7 I🚀

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,14 +1,20 @@
 import { Link, Route, Routes } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { LuPackageOpen } from "react-icons/lu";
 export default function AdminPage(){
     return(
-        <div className="flex h-screen w-full text-white">
-            <div className="w-90 h-full bg-red-900 flex flex-col">
-                <h1>Hi! Admin</h1>
-                <Link to="/admin">← Admin Dashboard</Link>
-                <Link to="/admin/users">User Management</Link>
-                <Link to="/admin/products">Products</Link>
+        <div className="flex h-screen w-full text-black shadow-2xl">
+            <div className="w-90 h-full flex flex-col">
+                <div className="w-full h-20 bg-blue-900 flex items-center justify-between px-4 mb-2">
+                    <img src="/logologin.png" alt="Logo" className="h-16 w-24 justify-between" />
+                    <span className="text-2xl font-bold text-amber-100">ADMIN</span>
+                </div>
+                <Link to="/admin" className="w-full flex items-center gap-2 text-2xl p-2 hover:bg-blue-700 hover:text-white"><FaShoppingCart className="text-2xl" />Orders</Link>
+                <Link to="/admin/users" className="w-full flex items-center gap-2 text-2xl p-2 hover:bg-blue-700 hover:text-white"><FaUser className="text-2xl" />User Management</Link>
+                <Link to="/admin/products" className="w-full flex items-center gap-2 text-2xl p-2 hover:bg-blue-700 hover:text-white"><LuPackageOpen className="text-2xl" />Products</Link>
             </div>
-            <div className="w-[calc(100%-360px)] h-full bg-yellow-300 text-black">
+            <div className="w-[calc(100%-360px)] h-full bg-gray-300 text-black">
                 <Routes>
                     <Route index element={<h1>Admin Dashboard</h1>} />
                     <Route path="/users" element={<h1>User Management</h1>} />
